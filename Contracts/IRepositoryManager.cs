329be5fdfb78bcc4e0f3b5023f07506c7fdca561
ahmadrz <a.hafizi9@outlook.com>
2023-10-10 18:28:24 +0330

    create repo-manager interface for keep intance of all repo classes

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts;
+public interface IRepositoryManager {
+    /*
+     * The output of some APis may be a combination 
+     * of data from several sources,
+     * in this case, we keep instances 
+     * from each repository and obtain 
+     * the desired data by combining them,
+     * for example, it may require a 
+     * combination of 5 or 6 models in our logic. 
+     */
+    IEmployeeRepository Employee { get; }
+    ICompanyRepository Company { get; }
+    void Save();
+}
+

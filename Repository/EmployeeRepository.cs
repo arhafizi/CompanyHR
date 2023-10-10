3dd8a3e27f299d92a0b5a1d71c7a6b5b3d44c96b
ahmadrz <a.hafizi9@outlook.com>
2023-10-10 18:18:18 +0330

    create reposBase-user classes

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,7 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository;
+public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository {
+    public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+}

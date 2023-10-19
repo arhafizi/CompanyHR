fbcf8198f2ab4116a5f517f84b7d583ce8881c2e
ahmadrz <a.hafizi9@outlook.com>
2023-10-19 22:22:02 +0330

    add presentation layers : create companies controller

---
+++ CompanyHR.Presentation/AssemblyReference.cs
@@ -0,0 +1,4 @@
+﻿namespace CompanyHR.Presentation;
+public static class AssemblyReference {
+
+}
---
+++ CompanyHR.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using System.Runtime.InteropServices;
+
+namespace CompanyHR.Presentation.Controllers {
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase {
+        private readonly IServiceManager _service;
+        public CompaniesController(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public IActionResult GetCompanies() {
+            try {
+                var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
+                return Ok(companies);
+            }
+            catch {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+    }
+}

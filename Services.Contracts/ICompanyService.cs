45954fdcb5065ab076a9bc29e952ab79927f03ee
ahmadrz <a.hafizi9@outlook.com>
2023-11-08 23:41:10 +0330

    add get by id method

--- Services.Contracts/ICompanyService.cs
+++ Services.Contracts/ICompanyService.cs
@@ -3,5 +3,6 @@
 namespace Services.Contracts;
 public interface ICompanyService {
     IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+    CompanyDto GetCompany(Guid companyId, bool trackChanges);
 
 }

6465676bed195030a5611cb8c6ab5bcdfed1e474
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:28:59 +0330

    update mapping profile

--- CompanyHR/MappingProfile.cs
+++ CompanyHR/MappingProfile.cs
@@ -8,5 +8,7 @@ public class MappingProfile : Profile {
     public MappingProfile() {
         CreateMap<Company, CompanyDto>()
             .ForCtorParam("FullAddress", opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+
+        CreateMap<Employee, EmployeeDto>();
     }
 }

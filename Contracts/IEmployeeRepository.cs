51a3b514df10ffb21396dde8b4b5c6fa0150a918
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:47:19 +0330

    add & implement get method to fetch one employee by id

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace Contracts;
 public interface IEmployeeRepository {
     IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
+    Employee GetEmployee(Guid companyId, Guid id, bool trackChanges);
 
 }
 
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,8 +7,14 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository 
 
     public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) {
 
-       return FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-            .OrderBy(e => e.Name)
-            .ToList();
+        return FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+             .OrderBy(e => e.Name)
+             .ToList();
+    }
+
+    public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) {
+
+        return FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
+            .SingleOrDefault();
     }
 }

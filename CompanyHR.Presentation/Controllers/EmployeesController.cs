f9ba1dd26a668acd49a3e7ce70cacc807347d6b4
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:30:02 +0330

    add GetEmployeesForCompany action method

---
+++ CompanyHR.Presentation/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace CompanyHR.Presentation.Controllers;
+
+[Route("api/companies/{companyId}/employees")]
+[ApiController]
+public class EmployeesController : ControllerBase {
+    private readonly IServiceManager _service;
+    public EmployeesController(IServiceManager service) {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetEmployeesForCompany(Guid companyId) {
+        var employees = _service.EmployeeService.GetEmployees(companyId, trackChanges: false);
+
+        return Ok(employees);
+    }
+
+}

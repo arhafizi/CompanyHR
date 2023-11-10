b6bc7673666767d7623704f06fbefdedde518f37
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:48:14 +0330

    add & implement get one employee by id , in service layer

--- Services.Contracts/IEmployeeService.cs
+++ Services.Contracts/IEmployeeService.cs
@@ -3,5 +3,7 @@
 namespace Services.Contracts;
 public interface IEmployeeService {
     IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+    EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
+
 }
 

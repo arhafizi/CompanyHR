c9d65739f92a31873a033d5fb2e08d08f5216936
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:28:22 +0330

    add & implement getAll employees in service layer

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,11 +1,8 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Shared.DataTransferObjects;
 
 namespace Service;
 internal class EmployeeService : IEmployeeService {
@@ -13,10 +10,22 @@ internal class EmployeeService : IEmployeeService {
     private readonly ILoggerManager _logger;
     private readonly IMapper _mapper;
 
-    public EmployeeService(IRepositoryManager repository, ILoggerManager logger , IMapper mapper) {
+    public EmployeeService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper) {
         _repository = repository;
         _logger = logger;
         _mapper = mapper;
+    }
+
+    public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges) {
+
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null)
+            throw new CompanyNotFoundException(companyId);
+
+        var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+        var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+
+        return employeesDto;
     }
 }
 
--- Services.Contracts/IEmployeeService.cs
+++ Services.Contracts/IEmployeeService.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shared.DataTransferObjects;
 
 namespace Services.Contracts;
 public interface IEmployeeService {
+    IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
 }
 

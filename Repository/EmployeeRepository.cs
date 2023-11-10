57f4ac0b2885897c9ad94a9e689a2a70730b9742
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:26:30 +0330

    add & implement get method to fetch all employees of a company

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -4,4 +4,11 @@ using Entities.Models;
 namespace Repository;
 public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository {
     public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+
+    public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) {
+
+       return FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            .OrderBy(e => e.Name)
+            .ToList();
+    }
 }

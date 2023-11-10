21d23ce89e867f124669cfbbd4b36030d38409a5
ahmadrz <a.hafizi9@outlook.com>
2023-11-11 00:49:02 +0330

    add getEmp by id , action method

--- CompanyHR.Presentation/Controllers/EmployeesController.cs
+++ CompanyHR.Presentation/Controllers/EmployeesController.cs
@@ -18,4 +18,11 @@ public class EmployeesController : ControllerBase {
         return Ok(employees);
     }
 
+    [HttpGet("{id:guid}")]
+    public IActionResult GetEmployeeForCompany(Guid companyId, Guid id) {
+
+        var employee = _service.EmployeeService.GetEmployee(companyId, id, trackChanges: false);
+
+        return Ok(employee);
+    }
 }

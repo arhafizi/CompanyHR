a70b2343ff2cc1e37ad934ca4f4c02177c259c8b
ahmadrz <a.hafizi9@outlook.com>
2023-11-13 23:57:16 +0330

    add & implement createEmployee method in service layer

--- Services.Contracts/IEmployeeService.cs
+++ Services.Contracts/IEmployeeService.cs
@@ -4,6 +4,7 @@ namespace Services.Contracts;
 public interface IEmployeeService {
     IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
     EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
-
+    EmployeeDto CreateEmployeeForCompany(Guid companyId,
+        EmployeeCreationDto employeeForCreation, bool trackChanges);
 }
 

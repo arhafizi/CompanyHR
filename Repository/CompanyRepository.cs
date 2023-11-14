2359327553e1f1b392d99e89a068194440a712c1
ahmadrz <a.hafizi9@outlook.com>
2023-11-14 23:50:31 +0330

    add & implement get CompanyCollection by id

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -5,15 +5,16 @@ namespace Repository;
 public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository {
     public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges) {
-        return FindAll(trackChanges)
-            .OrderBy(c => c.Name)
-            .ToList();
-    }
-    public Company GetCompany(Guid companyId, bool trackChanges) { 
-        return FindByCondition(c => c.Id.Equals(companyId), trackChanges)
-            .SingleOrDefault();
-    }
-    public void CreateCompany(Company company) => Create(company);
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+
+    public Company GetCompany(Guid companyId, bool trackChanges) =>
+         FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
+    
+    public void CreateCompany(Company company) => 
+        Create(company);
+    
+    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+        FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
 
 }

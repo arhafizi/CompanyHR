68e0f9e9819488eac7e811e3e572e616281ab04c
ahmadrz <a.hafizi9@outlook.com>
2023-11-14 23:51:26 +0330

    add GetCompanyCollection Action-Method

--- CompanyHR.Presentation/Controllers/CompaniesController.cs
+++ CompanyHR.Presentation/Controllers/CompaniesController.cs
@@ -42,4 +42,12 @@ public class CompaniesController : ControllerBase {
 
     }
 
+    [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids) {
+
+        var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+
+        return Ok(companies);
+    }
+
 }

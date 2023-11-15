05da63da6cd23eb17196bbad0f135e94dd32baa5
ahmadrz <a.hafizi9@outlook.com>
2023-11-15 21:20:39 +0330

    add supporting delete a employee for a company

--- CompanyHR.Presentation/Controllers/EmployeesController.cs
+++ CompanyHR.Presentation/Controllers/EmployeesController.cs
@@ -40,4 +40,12 @@ public class EmployeesController : ControllerBase {
             new { companyId, id = employeeToReturn.Id }, employeeToReturn);
     }
 
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id) {
+
+        _service.EmployeeService.DeleteEmployeeForCompany(companyId, id, trackChanges: false);
+        
+        return NoContent();
+    }
+
 }

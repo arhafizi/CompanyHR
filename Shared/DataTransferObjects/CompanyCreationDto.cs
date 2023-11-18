b89d15120be7f70ff175a28c0d694628894d01f2
ahmadrz <a.hafizi9@outlook.com>
2023-11-18 22:12:04 +0330

    use validation attributes & init setters

--- Shared/DataTransferObjects/CompanyCreationDto.cs
+++ Shared/DataTransferObjects/CompanyCreationDto.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects;
+public record CompanyCreationDto {
 
-public record CompanyCreationDto(string Name, string Address, string Country, 
-    IEnumerable<EmployeeCreationDto> Employees);
+    [Required(ErrorMessage = "company name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; init; }
+
+    [Required(ErrorMessage = "Address is a required field.")]
+    public string? Address { get; init; }
+    
+    [Required(ErrorMessage = "Country is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Country is 30 characters.")]
+    public string? Country { get; init; }
+
+    public IEnumerable<EmployeeCreationDto>? Employees {  get; init; }
+}

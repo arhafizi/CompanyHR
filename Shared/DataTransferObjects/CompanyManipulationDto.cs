ed7228e81643529d8ba89fbcc425d6ab5e961c29
ahmadrz <a.hafizi9@outlook.com>
2023-11-19 23:03:15 +0330

    extract base records for update & creation dtos

---
+++ Shared/DataTransferObjects/CompanyManipulationDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects;
+public abstract record CompanyManipulationDto {
+    [Required(ErrorMessage = "company name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; init; }
+
+    [Required(ErrorMessage = "Address is a required field.")]
+    public string? Address { get; init; }
+
+    [Required(ErrorMessage = "Country is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Country is 30 characters.")]
+    public string? Country { get; init; }
+
+    public IEnumerable<EmployeeCreationDto>? Employees { get; init; }
+}
